6261674c50948a9e5e0bdf4935660a60e0e6db60
neerajkeen17 <neeraj.kumar@fosteringlinux.com>
2024-04-23 18:43:42 +0530

    7

--- src/components/OTP/Otp.tsx
+++ src/components/OTP/Otp.tsx
@@ -1,3 +1,199 @@
+// "use client";
+// import Image from 'next/image';
+// import { useRouter } from 'next/navigation';
+// import { useRef, useState, useEffect } from 'react';
+// import axios from "axios";
+// import { useSelector } from 'react-redux';
+// import { RootState } from '@/store/store';
+
+// export default function OTP() {
+//   const { mobileNumber } = useSelector((state: RootState) => state.login);
+//   const router = useRouter();
+//   const [otp, setOtp] = useState(['', '', '', '']);
+//   const inputRefs = useRef([]);
+//   const [requestId, setRequestId] = useState('');
+
+//   useEffect(() => {
+//     const fetchOTP = async () => {
+//       try {
+//         const response = await axios.post('http://localhost:8080/http://10.15.15.247:81/esb/generate/otp',
+        
+//         {
+//           MethodId: '1',
+//           TellerID: '155',
+//           CustomerMobileNo: mobileNumber,
+//           EventId: '',
+//           VerifyParam: {},
+//           NotifyParam: {},
+//         });
+
+//         const data = response.data;
+
+//         if (data.ResponseCode === '000') {
+//           setRequestId(data.RequestId);
+//         } else {
+//           console.error('Error generating OTP:', data.DisplayMessage);
+//         }
+//       } catch (error) {
+//         console.error('Error generating OTP:', error);
+//       }
+//     };
+
+//     fetchOTP();
+//   }, [mobileNumber]);
+
+//   console.log(" dekh requestId aaya ki nahi ",requestId);
+
+//   const handleSubmit = async () => {
+//       const otpValue = otp.join('');
+//       if (otpValue.length === 4 && /^[0-9]+$/.test(otpValue)) {
+//         console.log("OTP value", otpValue);
+//         try {
+//           const response = await axios.post(
+//             'http://localhost:8080/http://10.15.15.247:81/esb/smsservice',
+//             {
+//               MethodId: '2',
+//               TellerID: '155',
+//               CustomerMobileNo: mobileNumber,
+//               EventId: '',
+//               VerifyParam: {
+//                 RequestId: requestId,
+//                 OtpPin: otpValue,
+//               },
+//               NotifyParam: {},
+//             },
+//             {
+//               headers: {
+//                 'X-Correlation-Id': '123456789_01012024120102020',
+//               },
+//             }
+//           );
+    
+//           const data = response.data;
+//           console.log("gydefyiyie", response.data.ResponseCode);
+    
+//           if (data.ResponseCode === '000') {
+//             router.push('/dashboard');
+//           } else {
+//             console.error('OTP verification failed:', data.DisplayMessage);
+//           }
+//         } catch (error) {
+//           console.error('Error verifying OTP:', error);
+//         }
+//       }
+//   };
+
+//   const handleResend = async () => {
+//     setOtp(['', '', '', '']);
+//     try {
+//       const response = await axios.post('http://localhost:8080/http://10.15.15.247:81/esb/generate/otp', 
+//       {
+//         MethodId: '1',
+//         TellerID: '155',
+//         CustomerMobileNo: mobileNumber,
+//         EventId: '',
+//         VerifyParam: {},
+//         NotifyParam: {},
+//       });
+
+//       const data = response.data;
+
+//       if (data.ResponseCode === '000') {
+//         setRequestId(data.RequestId);
+//       } else {
+//         console.error('Error generating OTP:', data.DisplayMessage);
+//       }
+//     } catch (error) {
+//       console.error('Error generating OTP:', error);
+//     }
+//   };
+
+//   const handleChange = (e, idx) => {
+//     const value = e.target.value;
+//     if (/^[0-9]?$/.test(value) || value === '') {
+//       const newOtp = [...otp];
+//       newOtp[idx] = value;
+//       setOtp(newOtp);
+//       if (value === '') {
+//         // If the user deletes the input, move the focus to the previous input
+//         if (idx > 0) {
+//           inputRefs.current[idx - 1].focus();
+//         }
+//       } else if (idx < 3) {
+//         // If the user enters a digit, move the focus to the next input
+//         inputRefs.current[idx + 1].focus();
+//       }
+//     }
+//   };
+
+//   return (
+//     <main className="h-screen flex-col items-center justify-between bg-[#d5d4f6]">
+//       <div className="flex bg-white justify-between p-2 mx-1 mb-1 rounded-xl">
+//         <Image src="/fino.jpg" alt="Fino" width={300} height={300} />
+//         <div>
+//           <div className="text-blue-800 font-bold">Qadar</div>
+//           <div>
+//             <span className="text-red-500 font-bold mr-1">Apki Mehnat</span>
+//             <span className="text-blue-800 font-bold">Ki</span>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="flex flex-col items-center space-y-6 mt-20">
+//         <div className="border border-dashed border-purple-800 p-5">
+//           <h1 className="text-4xl font-extrabold">Enter One Time Pin</h1>
+//         </div>
+//         <div>
+//           {otp.map((digit, idx) => (
+//             <input
+//               key={idx}
+//               type="text"
+//               maxLength="1"
+//               value={digit}
+//               onChange={(e) => handleChange(e, idx)}
+//               ref={(el) => (inputRefs.current[idx] = el)}
+//               className="bg-white h-14 w-14 m-4 p-4 text-2xl font-bold rounded-lg"
+//             />
+//           ))}
+//         </div>
+//         <div className="flex flex-row justify-between gap-24">
+//           <button
+//             className="bg-[#aca7a7] mx-2 rounded-lg text-lg font-bold py-4 px-10"
+//             onClick={handleSubmit}
+//           >
+//             Submit
+//           </button>
+//           <button
+//             className="bg-[#aca7a7] mx-2 rounded-lg text-lg font-bold py-4 px-5"
+//             onClick={handleResend}
+//           >
+//             Resend OTP
+//           </button>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use client";
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -5,6 +201,7 @@ import { useRef, useState, useEffect } from 'react';
 import axios from "axios";
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
+import { MdError } from "react-icons/md";
 
 export default function OTP() {
   const { mobileNumber } = useSelector((state: RootState) => state.login);
@@ -12,6 +209,7 @@ export default function OTP() {
   const [otp, setOtp] = useState(['', '', '', '']);
   const inputRefs = useRef([]);
   const [requestId, setRequestId] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchOTP = async () => {
@@ -40,47 +238,55 @@ export default function OTP() {
     };
 
     fetchOTP();
-  }, []);
+  }, [mobileNumber]);
 
-  console.log(" dekh requestId aaya ki nahi ",requestId);
+  console.log(" RequestID which comes from OTP generate API ",requestId);
 
   const handleSubmit = async () => {
-    const otpValue = otp.join('');
-    if (otpValue.length === 4 && /^[0-9]+$/.test(otpValue)) {
-      try {
-        const response = await axios.post('http://localhost:8080/http://10.15.15.247:81/esb/smsservice', {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          data: JSON.stringify({
-            MethodId: '2',
-            TellerID: '155',
-            CustomerMobileNo: mobileNumber,
-            EventId: '',
-            VerifyParam: {
-              RequestId: requestId,
-              OtpPin: otpValue,
+      const otpValue = otp.join('');
+      if (otpValue.length === 4 && /^[0-9]+$/.test(otpValue)) {
+        console.log("OTP value", otpValue);
+        try {
+          const response = await axios.post(
+            'http://localhost:8080/http://10.15.15.247:81/esb/smsservice',
+            {
+              MethodId: '2',
+              TellerID: '155',
+              CustomerMobileNo: mobileNumber,
+              EventId: '',
+              VerifyParam: {
+                RequestId: requestId,
+                OtpPin: otpValue,
+              },
+              NotifyParam: {},
             },
-            NotifyParam: {},
-          }),
-        });
-
-        const data = response.data;
-
-        if (data.ResponseCode === '000') {
-          router.push('/dashboard');
-        } else {
-          console.error('OTP verification failed:', data.DisplayMessage);
+            {
+              headers: {
+                'X-Correlation-Id': '123456789_01012024120102020',
+              },
+            }
+          );
+    
+          const data = response.data;
+          console.log("gydefyiyie", response.data.ResponseCode);
+    
+          if (data.ResponseCode === '000') {
+            router.push('/dashboard');
+          } else {
+            setErrorMessage(data.DisplayMessage);
+          }
+        } catch (error) {
+          console.error('Error verifying OTP:', error);
         }
-      } catch (error) {
-        console.error('Error verifying OTP:', error);
       }
-    }
   };
 
   const handleResend = async () => {
+    setOtp(['', '', '', '']);
+    setErrorMessage('');
     try {
-      const response = await axios.post('http://localhost:8080/http://10.15.15.247:81/esb/generate/otp', {
+      const response = await axios.post('http://localhost:8080/http://10.15.15.247:81/esb/generate/otp', 
+      {
         MethodId: '1',
         TellerID: '155',
         CustomerMobileNo: mobileNumber,
@@ -131,6 +337,14 @@ export default function OTP() {
           </div>
         </div>
       </div>
+
+      {errorMessage && (
+        <div className="flex bg-white rounded-2xl p-2">
+          <MdError size={22} />
+          <span className="ml-2 text-black font-semibold pr-2">{errorMessage}</span>
+        </div>
+      )}
+
       <div className="flex flex-col items-center space-y-6 mt-20">
         <div className="border border-dashed border-purple-800 p-5">
           <h1 className="text-4xl font-extrabold">Enter One Time Pin</h1>
